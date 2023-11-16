53de158ad7c5e2d0acb38de1258688cb08a386f1
HeloiseOliveira <heloise.oliveiradasilva@outlook.com>
2023-11-16 15:45:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using exemplos_dotnet.Models;
+
+Pessoa p1 = new Pessoa(nome: "Bruno", sobrenome: "Silva");
+
+Pessoa p2 = new Pessoa(nome: "Juliana", sobrenome: "Cardeal");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Bruno";
+//p1.Idade = 20;
+//p1.Apresentar();
+
+
